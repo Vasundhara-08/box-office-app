c69a2168a36948ec5ddf04c2a60785b967c4f33f
Vasundhara Vasu <76790590+Vasundhara-08@users.noreply.github.com>
2022-07-29 23:17:11 +0530

    installed new project and dev dep

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
@@ -34,6 +35,7 @@ export function register(config) {
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
+        // eslint-disable-next-line no-use-before-define
         checkValidServiceWorker(swUrl, config);
 
         // Add some additional logging to localhost, pointing developers to the
@@ -46,6 +48,7 @@ export function register(config) {
         });
       } else {
         // Is not localhost. Just register service worker
+        // eslint-disable-next-line no-use-before-define
         registerValidSW(swUrl, config);
       }
     });
